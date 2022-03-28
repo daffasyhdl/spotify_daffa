17160e2b34645bd683bc9042d4e502c3de9672c3
daffasyhdl <92908244+daffasyhdl@users.noreply.github.com>
2022-03-28 13:11:47 +0700

    Update index.jsx

--- src/components/data1/index.jsx
+++ src/components/data1/index.jsx
@@ -1,10 +1,17 @@
 import React from "react";
-const Gif = ({ url, title }) => {
+
+const Playlist = ({ key, url, title, artist, album }) => {
   return (
-    <>
-      <h3>{title}</h3>
-      <img src={url} alt="gif" />
-    </>
+      <div className="playlist">
+          <div className='container' key={key}>
+              <h4 className='title'>{title}</h4>
+              <img className='image' src={url} alt="image" />
+              <h4 className='artist'>{artist}</h4>
+              <h6 className='album'>{album}</h6>
+              <Button className='btn-select' variant="outline-primary">Select</Button>{' '}
+          </div>
+      </div>
   );
-};
-export default Gif;
+}
+
+export default Playlist
